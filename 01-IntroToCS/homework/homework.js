8823ccf0dc1749fd7496c025f3448e6c629b67b3
Ivan Ramos <huramos777@gmail.com>
2022-09-13 14:58:31 -0300

    TAREA FINALIZADA

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,34 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
+  let exp = num.length - 1;
+  console.log(exp)
+  
+  
+  for (let i = 0; i < num.length; i++) {
+    if(num[i] == 1){
+      sum = sum + Math.pow(2, exp)
+    }
+    exp = exp - 1;    
+  }
+  return sum;
 
+  
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let resultBinaryStr = '' //declaro variable con string vacio para asegurarme que es string
+  let result = 0 //inicializo variable donde van los restos de las divisiones
+  //inicio bucle mientras el numero sea distinto de 0 (hasta que llegue a 0)
+  while(num !== 0){
+    result = num % 2 //calculo resto
+    num = Math.floor(num / 2) //calculo nuevo numero para siguiente bucle
+    resultBinaryStr = result + resultBinaryStr // resultado al inicio del string 
+  }
 
+  return resultBinaryStr
 }
 
 
